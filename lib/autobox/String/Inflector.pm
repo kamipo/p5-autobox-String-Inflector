bb101bf1fbfcb666e417439769bb4fa8f72e9f19
kamipo <kamipo@gmail.com>
2009-02-24 02:04:49 +0900

    initial commit

---
+++ lib/autobox/String/Inflector.pm
@@ -0,0 +1,33 @@
+package autobox::String::Inflector;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+autobox::String::Inflector -
+
+=head1 SYNOPSIS
+
+  use autobox::String::Inflector;
+
+=head1 DESCRIPTION
+
+autobox::String::Inflector is
+
+=head1 AUTHOR
+
+Ryuta Kamizono E<lt>kamipo@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
